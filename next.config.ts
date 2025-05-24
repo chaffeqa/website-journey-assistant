1e562b20cb71e6c7da3e28a17196c4cd9deae4b5
Quinn <chaffeqa@gmail.com>
2025-05-24 13:19:42 -0600

    Removed local CRF protection

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['*.finalforms-amp.me'],
 };
 
 export default nextConfig;
